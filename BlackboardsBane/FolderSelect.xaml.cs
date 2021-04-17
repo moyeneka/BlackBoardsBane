33f70ff034f0aa1672878e8ad60407b230d0fe23
scanales01 <42147541+scanales01@users.noreply.github.com>
2021-04-17 12:35:34 -0500

    Folder Select Window created

---
+++ BlackboardsBane/FolderSelect.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace BlackboardsBane
+{
+    /// <summary>
+    /// Interaction logic for FolderSelect.xaml
+    /// </summary>
+    public partial class FolderSelect : Window
+    {
+        public FolderSelect()
+        {
+            InitializeComponent();
+        }
+
+        private void browseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = folderName.Text;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                folderName.Text = dialog.FileName;
+            }
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void selectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
